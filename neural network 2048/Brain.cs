64103ae89f827c70038d79efedd8dc83d501ac89
JamesFrowen-PC\JamesFrowen <jamesfrowen@talktalk.net>
2016-10-12 21:38:22 +0100

    removed 4th hidden, changed mutaltion rate

--- neural network 2048/Brain.cs
+++ neural network 2048/Brain.cs
@@ -11,24 +11,26 @@ namespace neural_network_2048
         public static Random r;
         public static int NextID;
         //brain with 1 hidden layer
-        public Brain(int input=18, int hidden1=18, int hidden2 = 8, int output=4)
+        public Brain(int input = 18, int hidden1 = 18, int hidden2 = 12, int hidden3 = 8, int output = 4)
         {
             Input = new Matrix(input, 1);
             Hidden1 = new Matrix(hidden1, 1);
             Hidden2 = new Matrix(hidden2, 1);
+            Hidden3 = new Matrix(hidden3, 1);
             Output = new Matrix(output, 1);
-            
+
 
             ID = NextID;
             NextID++;
         }
         public void Initnew()
         {
-            Mutability = r.NextDouble()*0.1;
+            //Mutability = r.NextDouble() * 0.1;
 
             Weight1 = new Matrix(Hidden1.Rows, Input.Rows);
             Weight2 = new Matrix(Hidden2.Rows, Hidden1.Rows);
-            Weight3 = new Matrix(Output.Rows, Hidden2.Rows);
+            Weight3 = new Matrix(Hidden3.Rows, Hidden2.Rows);
+            Weight4 = new Matrix(Output.Rows, Hidden3.Rows);
             for (int i = 0; i < Weight1.Rows; i++)
             {
                 for (int j = 0; j < Weight1.Columns; j++)
@@ -36,7 +38,7 @@ namespace neural_network_2048
                     Weight1.Data[i][j] = r.NextDouble() * 4 - 2;
                 }
             }
-            for (int i = 0; i < Weight2.Rows ; i++)
+            for (int i = 0; i < Weight2.Rows; i++)
             {
                 for (int j = 0; j < Weight2.Columns; j++)
                 {
@@ -50,44 +52,56 @@ namespace neural_network_2048
                     Weight3.Data[i][j] = r.NextDouble() * 4 - 2;
                 }
             }
+            for (int i = 0; i < Weight4.Rows; i++)
+            {
+                for (int j = 0; j < Weight4.Columns; j++)
+                {
+                    Weight4.Data[i][j] = r.NextDouble() * 4 - 2;
+                }
+            }
+            
 
             Bias1 = r.NextDouble() * 2 - 1;
             Bias2 = r.NextDouble() * 2 - 1;
             Bias3 = r.NextDouble() * 2 - 1;
+            Bias4 = r.NextDouble() * 2 - 1;
         }
         Matrix Input;
         Matrix Hidden1;
         Matrix Hidden2;
+        Matrix Hidden3;
         Matrix Output;
 
         Matrix Weight1;
         Matrix Weight2;
         Matrix Weight3;
+        Matrix Weight4;
 
         double Bias1;
         double Bias2;
         double Bias3;
+        double Bias4;
 
         public void CalculateMove(Puzzle P, bool forceMove = false)
         {
             // give data to input matrix
             double m = Math.Log(P.HighestOnGrid(), 2);
-            for (int x=0;x<P.Width;x++)
+            for (int x = 0; x < P.Width; x++)
             {
                 for (int y = 0; y < P.Height; y++)
                 {
                     double v = P.Grid[x, y] == 0 ? -1 : Math.Log(P.Grid[x, y], 2);
-                    Input.Data[x * P.Height + y][ 0] = v / m;
+                    Input.Data[x * P.Height + y][0] = v / m;
                 }
             }
             //ActivationFunction(Input);
-            Input.Data[16][ 0] = P.BadMove();
-            Input.Data[17][ 0] = P.EmptyPercent();
+            Input.Data[16][0] = P.BadMove();
+            Input.Data[17][0] = P.EmptyPercent();
 
             if (forceMove && P.EmptyPercent() > 0.7) // force moves at start
             {
                 Output.Data[0][0] = 0;//x1
-                Output.Data[1][ 0] = 1;//y1
+                Output.Data[1][0] = 1;//y1
                 Output.Data[2][0] = 1;//x2
                 Output.Data[3][0] = 0;//y2
             }
@@ -96,10 +110,11 @@ namespace neural_network_2048
                 // calculate output
                 Hidden1.ActivationFromMultiply(Weight1, Input, Bias1);
                 Hidden2.ActivationFromMultiply(Weight2, Hidden1, Bias2);
-                Output.ActivationFromMultiply(Weight3, Hidden2, Bias3);
+                Hidden3.ActivationFromMultiply(Weight3, Hidden2, Bias3);
+                Output.ActivationFromMultiply(Weight4, Hidden3, Bias4);
             }
 
-            
+
         }
         public void MakeMove(Puzzle P)
         {
@@ -107,18 +122,18 @@ namespace neural_network_2048
             if (P.DoForceMove)
             {
                 x = Output.Data[2][0];
-                y = Output.Data[3][ 0];
-            } 
+                y = Output.Data[3][0];
+            }
             else
             {
                 x = Output.Data[0][0];
                 y = Output.Data[1][0];
             }
-            
 
-            if (x*x > y*y)
+
+            if (x * x > y * y)
             { // x bigger
-                if (x>0)
+                if (x > 0)
                 { // positive  
                     P.Move(Puzzle.MoveDirection.right);
                 }
@@ -157,32 +172,36 @@ namespace neural_network_2048
         public int ParentID;
         public int Generation = 0;
 
-        public double Mutability;
-        private double MaxMuta = 0.2;
-        private double MinMuta = 0.001;
+        //public double Mutability;
+        //private double MaxMuta = 0.2;
+        //private double MinMuta = 0.001;
         /// <summary>
         /// how much less mutability will change compated to other vars.
         /// </summary>
-        private double DeltaMuta = 4;
+        //private double DeltaMuta = 4;
 
         public Brain Evolve()
         {
-            Brain child = new Brain(Input.Rows, Hidden1.Rows, Hidden2.Rows, Output.Rows);
+            Brain child = new Brain(Input.Rows, Hidden1.Rows, Hidden2.Rows, Hidden3.Rows, Output.Rows);
             child.Weight1 = MatrixEvolve(Weight1);
             child.Weight2 = MatrixEvolve(Weight2);
             child.Weight3 = MatrixEvolve(Weight3);
+            child.Weight4 = MatrixEvolve(Weight4);
             child.Bias1 = Bias1 * Math.Pow(1.05, r.NextDouble() - 0.5);
             if (Math.Abs(child.Bias1) < 0.01 * r.NextDouble())
-            { child.Bias1 *= -1;  }
+            { child.Bias1 *= -1; }
             child.Bias2 = Bias2 * Math.Pow(1.05, r.NextDouble() - 0.5);
             if (Math.Abs(child.Bias2) < 0.01 * r.NextDouble())
             { child.Bias2 *= -1; }
             child.Bias3 = Bias3 * Math.Pow(1.05, r.NextDouble() - 0.5);
             if (Math.Abs(child.Bias3) < 0.01 * r.NextDouble())
             { child.Bias3 *= -1; }
+            child.Bias4 = Bias4 * Math.Pow(1.05, r.NextDouble() - 0.5);
+            if (Math.Abs(child.Bias4) < 0.01 * r.NextDouble())
+            { child.Bias4 *= -1; }
             //child.Mutability = Mutability + (r.NextDouble() - 0.5) * Mutability / DeltaMuta;
-            if (child.Mutability > MaxMuta) { child.Mutability = MaxMuta; }
-            if (child.Mutability < MinMuta) { child.Mutability = MinMuta; }
+            //if (child.Mutability > MaxMuta) { child.Mutability = MaxMuta; }
+            //if (child.Mutability < MinMuta) { child.Mutability = MinMuta; }
 
             child.Generation = Generation + 1;
             child.ParentID = ID;
@@ -195,8 +214,16 @@ namespace neural_network_2048
             {
                 for (int j = 0; j < R.Columns; j++)
                 {
-                    R.Data[i][j] = R.Data[i][j] * Math.Pow(1.05, r.NextDouble() - 0.5);
-                    if (Math.Abs(R.Data[i][j]) < 0.01 * r.NextDouble())
+                    // only mutate at 10% chance
+                    if (r.NextDouble() < 0.1) 
+                    {
+                        R.Data[i][j] = R.Data[i][j] * Math.Pow(1.05, r.NextDouble() - 0.5);
+                    }
+                    // only mutate (flip sign) at 0.01%/value
+                    // examples:
+                    // if value = 0.01 then there is 10% chance of fliping sign
+                    // if value is 0.1 then there is a 1% chance of flipping
+                    if (r.NextDouble() < 0.001/ R.Data[i][j]) 
                     { R.Data[i][j] *= -1; }
                 }
             }
